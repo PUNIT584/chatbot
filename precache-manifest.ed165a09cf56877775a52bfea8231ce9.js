8f8e3b98d03993643e122058d660193355b8f8a1
Punit Dhaked <punitdhaked584@gmail.com>
2024-09-30 01:26:49 +0530

    Updates

---
+++ precache-manifest.ed165a09cf56877775a52bfea8231ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8803c575ffd53659a61f11b1ed1f9ff7",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "ec3f881403b9d503e324",
+    "url": "/chatbot/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e25fb48e4fb1f7b6e46f",
+    "url": "/chatbot/static/js/2.cbee99d1.chunk.js"
+  },
+  {
+    "revision": "6a2a939c6c472481a460",
+    "url": "/chatbot/static/js/3.5a11cc32.chunk.js"
+  },
+  {
+    "revision": "ec3f881403b9d503e324",
+    "url": "/chatbot/static/js/main.8a19041a.chunk.js"
+  },
+  {
+    "revision": "002f8af93e952eedbc92",
+    "url": "/chatbot/static/js/runtime~main.8aa4ff25.js"
+  }
+]);
